82728828d8ec5c6dacaedc6c2e1c263d81932a9f
jbarakat <jbarakat@stanford.edu>
2016-02-10 16:38:22 -0800

    edited func and bcond

--- C++/draft01/ALT/allCa/include/bcond.h
+++ C++/draft01/ALT/allCa/include/bcond.h
@@ -7,8 +7,7 @@
  *  
  * PARAMETERS
  *  n			[input]		number of ODEs
- *  area	[input]		total surface area (parameter #1)
- *  vlme	[input]		total volume (parameter #2)
+ *  par   [input]   parameters
  *  A			[output]	matrix of coefficients at t = 0
  *  B			[output]	matrix of coefficients at t = 1
  *  c			[output]	right-hand side vector
@@ -20,6 +19,7 @@
 
 /* HEADER FILES */
 #include <math.h>
+#include <gsl/gsl_sf_trig.h>
 
 /* PROTOTYPES */
 void bcond(int, double, double, double*, double*, double*);
@@ -27,11 +27,22 @@ void bcond(int, double, double, double*, double*, double*);
 /* IMPLEMENTATIONS */
 /* Calculate coefficients for linear BC
  *   r = A*y(0) + B*y(1) - c = 0 */
-void bcond(int n, double area, double vlme,
+void bcond(int n, double par[6],
            double *A, double *B, double *c){
   int i; // for (i+1)st BC
   int j; // for jth element of y
   double PIH = M_PI/2.0;
+	
+	// parameters
+	double T0   = par[0]; // membrane relaxation time = mu*a^3/kappa
+	double alph = par[1]; // taper angle of tube wall
+	double v    = par[2]; // reduced volume
+	double dp   = par[3]; // pressure drop (scaled by kappa/a^3)
+	double R0   = par[4]; // tube radius at center-of-mass axial position (scaled by a)
+	double xcom = par[5]; // center-of-mass position 
+	                      //   = time integral of center-of-mass translational speed
+
+	double tana = gsl_sf_sin(alph)/gsl_sf_cos(alph);
   
   // initialize
   for (i = 0; i < n; i++){
@@ -43,24 +54,31 @@ void bcond(int n, double area, double vlme,
   }
 
   // matrix of coefficients for BC at t = 0
-  A[0*n + 0] = 1; // r  (0) = 0
-  A[1*n + 1] = 1; // psi(0) = -M_PI/2
-  A[2*n + 3] = 1; // qs (0) = 0
-  A[3*n + 6] = 1; // A  (0) = 0
-  A[4*n + 7] = 1; // V  (0) = 0
+  A[0 *n + 0 ] =  1   ; // r  (0) = 0
+  A[1 *n + 2 ] =  1   ; // psi(0) = -PIH
+  A[2 *n + 4 ] =  1   ; // qs (0) = 0
+  A[3 *n + 7 ] =  1   ; // A  (0) = 0
+  A[4 *n + 8 ] =  1   ; // V  (0) = 0
+  A[5 *n + 10] =  1   ; // R  (0) + x(0)*tana = R0
+  A[5 *n + 1 ] =  tana; 
+  A[6 *n + 11] =  1   ; // X  (0) = 0
+  A[7 *n + 5 ] =  1   ; // p  (0) - p(1) = dp
   
   // matrix of coefficients for BC at t = 1
-  B[5*n + 0] = 1; // r  (1) = 0
-  B[6*n + 1] = 1; // psi(1) = M_PI/2
-  B[7*n + 3] = 1; // qs (1) = 0 
-  B[8*n + 6] = 1; // A  (1) = area
-  B[9*n + 7] = 1; // V  (1) = vlme
+  B[7 *n + 5 ] = -1; // p  (0) - p(1) = dp
+  B[8 *n + 0 ] =  1; // r  (1) = 0
+  B[9 *n + 2 ] =  1; // psi(1) = PIH
+  B[10*n + 4 ] =  1; // qs (1) = 0 
+  B[11*n + 7 ] =  1; // A  (1) = 4.0*M_PI
+  B[12*n + 8 ] =  1; // V  (1) = 4.0*M_PI*v/3.0
+  B[13*n + 11] =  1; // X  (1) = xcom
 
   // right-hand side vector
-  c[1] = -PIH;
-  c[6] =  PIH;
-  c[8] =  area;
-  c[9] =  vlme;
+  c[2 ] = -PIH           ;
+  c[5 ] =  R0            ;
+  c[9 ] =  PIH           ;
+  c[11] =  4.0*M_PI      ;
+  c[12] =  4.0*M_PI*v/3.0;
 }
 
 
--- C++/draft01/ALT/allCa/include/func.h
+++ C++/draft01/ALT/allCa/include/func.h
@@ -6,19 +6,11 @@
  *  
  * PARAMETERS
  *  ny					[input]		number of ODEs
+ *  par         [input]   parameters
+ *  ur					[input]		velocity of radius of curvature
  *  t						[input]		abscissas
  *  y						[input]		solution
- *  f						[input]		function
- *  r   = y[0]  [input]		radius
- *  psi = y[1]  [input]		tilt angle
- *  cs  = y[2]  [input]		meridional curvature
- *  qs  = y[3]  [input]		transverse shear tension
- *  p   = y[4]  [input]		pressure
- *  sig = y[5]  [input]		mean tension
- *  A   = y[6]  [input]		total surface area
- *  V   = y[7]  [input]		total volume
- *  Q   = y[8]  [input]		leakback flow rate
- *  S   = y[9]  [input]		total meridional arc length
+ *  f						[output]	function
  */
 
 #ifndef FUNC_H
@@ -38,103 +30,104 @@ using namespace std;
 /* PROTOTYPES */
 
 /* IMPLEMENTATIONS */
-void func(int ny, double Ca,
+void func(int ny, double par[6], double ur,
           double t, double *y, double *f){
+	if (ny != 14)
+    cout << "Error: ny should equal 14." << endl;
+
   int i;
   
-  if (ny != 10)
-    cout << "Error: ny should equal 10." << endl;
+	// characteristic scales:
+	//  - a     = nominal radius of vesicle
+	//  - kappa = bending modulus
+	//  - mu    = suspending fluid viscosity
+
+	// parameters
+	double T0   = par[0]; // membrane relaxation time = mu*a^3/kappa
+	double alph = par[1]; // taper angle of tube wall
+	double v    = par[2]; // reduced volume
+	double dp   = par[3]; // pressure drop (scaled by kappa/a^3)
+	double R0   = par[4]; // tube radius at center-of-mass axial position (scaled by a)
+	double xcom = par[5]; // center-of-mass position 
+	                      //   = time integral of center-of-mass translational speed
+
+
+	double tana = gsl_sf_sin(alph)/gsl_sf_cos(alph);
   
   // define variables
-  double r   = y[0];
-  double psi = y[1];
-  double cs  = y[2];
-  double qs  = y[3];
-  double p   = y[4];
-  double sig = y[5];
-  double A   = y[6];
-  double V   = y[7];
-  double Q2  = y[8];
-  double S   = y[9];
+  double r   = y[0 ]; // azimuthal radius of curvature		(scaled by a)
+  double x   = y[1 ]; // axial position										(scaled by a)
+  double psi = y[2 ]; // tilt angle 
+  double cs  = y[3 ]; // meridional curvature							(scaled by 1/a)
+  double qs  = y[4 ]; // transverse shear tension         (scaled by kappa/a^2)
+  double p   = y[5 ]; // gap pressure                     (scaled by kappa/a^3)
+  double sig = y[6 ]; // mean in-plane tension            (scaled by kappa/a^2)
+  double A   = y[7 ]; // surface area                     (scaled by a^2)
+  double V   = y[8 ]; // volume                           (scaled by a^3)
+  double Q   = y[9 ]; // leakback flux per unit circumf.  (scaled by kappa/(mu*a))
+  double R   = y[10]; // tube radius                      (scaled by a)
+	double X   = y[11]; // center-of-mass axial position    (scaled by x)
+  double U   = y[12]; // center-of-mass axial speed       (scaled by kappa/(mu*a^2))
+  double S   = y[13]; // meridional arc length            (scaled by a) 
 	
-	// check bounds on radius
-	if (r > 0.99999999)
-		r = 0.99999999;
+	// check bounds on radius of curvature
+	if (r > 0.99999999*R)
+		r = 0.99999999*R;
 	
 	if (r <= 0)
 		r = 1e-12;
 	
 	/*-------- RESULTS FROM LUBRICATION THEORY --------*/
 	
-	double Dr   = 1.0 - r  ; 
+	double Dr   = R - r    ; 
+	double iDr  = 1.0 /Dr  ; 
+	double iDr2 = iDr*iDr  ; 
   double Dr2  = Dr  *Dr  ; 
   double Dr3  = Dr  *Dr2 ;
   double Dr4  = Dr2 *Dr2 ;
-  double Dr5  = Dr2 *Dr3 ;
-  double Dr6  = Dr3 *Dr3 ;
-  double Dr7  = Dr3 *Dr4 ;
-  double Dr8  = Dr4 *Dr4 ;
-  double Dr9  = Dr4 *Dr5 ;
-  double Dr10 = Dr5 *Dr5 ;
-  double Dr11 = Dr5 *Dr6 ;
-  double Dr12 = Dr6 *Dr6 ;
-  double Dr13 = Dr6 *Dr7 ;
-  double Dr14 = Dr7 *Dr7 ;
-  double Dr15 = Dr7 *Dr8 ;
-  double Dr16 = Dr8 *Dr8 ;
-  double Dr17 = Dr8 *Dr9 ;
-  double Dr18 = Dr9 *Dr9 ;
-  double Dr19 = Dr9 *Dr10;
-  double Dr20 = Dr10*Dr10;
-  	
-	double r2  = r*r;
+  
+	double r2  = r *r ;
+	double R2  = R *R ;
+	double R3  = R *R2;
+	double R4  = R2*R2;
+	double R5  = R2*R3;
+	double R6  = R3*R3;
+
+	double Q2  = 2.0*Q*R0/R;
+	double UR  = U*R;
   double g, e;
   
 	// calculate local axial pressure gradient, g = (dp/dx)/Ca
 	if (r > 0.0001){
-  	double log = gsl_sf_log(r  );
+  	double log = gsl_sf_log(r/R);
 
-		g  = (8.0/(1.0 - r2));
-		g *= (Q2 - 1.0 - (1.0 - r2)/(2.0*log));
-		g /= (1.0 + r2 + (1.0 - r2)/log);
+		g  = (8.0/(R2 - r2));
+		g *= (Q2*R - U*(R2 + (R2 - r2)/(2.0*log)));
+		g /= (R2 + r2 + (R2 - r2)/log);
 	}
 	else {
-		g  = 8.0*(Q2 - 1.0); // limiting value as r --> 0
+		g  = 8.0*(Q2 - UR)/R3; // limiting value as r --> 0
 	}
 
 	// calculate local membrane shear rate, e = du/dr
-//	if (r > 0.02){
-//  	double log = gsl_sf_log(r);
-//		double rlog = r*log;
-//		double irlog = 1.0/rlog;
-//
-//		e = 0.25*g*((1 - r2)*irlog + 2.0*r) + irlog;
-//	}
-//	else {
-  	e  = -(3.0/Dr2)*Q2                ;
-  	e +=  (2.0/Dr )*(1.0 - Q2)        ;
-  	e +=       1.0      - 1.45    *Q2 ;
-  	e += Dr  *(0.7      - 1.15    *Q2);
-  	e += Dr2 *(0.55     - 0.981786*Q2);
-  	e += Dr3 *(0.469286 - 0.882857*Q2);
-  	e += Dr4 *(0.423214 - 0.820839*Q2);
-//		e += Dr5 *(0.395036 - 0.778982*Q2); // higher order terms just
-//		e += Dr6 *(0.376375 - 0.748554*Q2); // emphasize the singular
-//		e += Dr7 *(0.362969 - 0.724934*Q2); // nature of the solution
-//		e += Dr8 *(0.352610 - 0.705622*Q2); // near the poles, which is
-//		e += Dr9 *(0.344131 - 0.689218*Q2); // unphysical ...
-//		e += Dr10*(0.336895 - 0.674906*Q2);
-//		e += Dr11*(0.330541 - 0.662184*Q2);
-//		e += Dr12*(0.324853 - 0.650726*Q2);
-//		e += Dr13*(0.319695 - 0.640305*Q2);
-//		e += Dr14*(0.314973 - 0.630758*Q2);
-//		e += Dr15*(0.310620 - 0.621958*Q2);
-//		e += Dr16*(0.306587 - 0.613808*Q2);
-//		e += Dr17*(0.302833 - 0.606228*Q2);
-//		e += Dr18*(0.299326 - 0.599152*Q2);
-//		e += Dr19*(0.296039 - 0.592525*Q2);
-//		e += Dr20*(0.292949 - 0.586299*Q2);
-//	}
+
+	// EXACT FORM:
+  // double log = gsl_sf_log(r/R);
+	// double rlog = r*log;
+	// double irlog = 1.0/rlog;
+	// e = 0.25*g*((R2 - r2)*irlog + 2.0*r) + U*irlog;
+  	
+	// TAYLOR SERIES ABOUT r = R:
+	// (to avoid the singularity at r = 0)
+	e  = iDr2 *(            - 3.0     *Q2)   ;
+  e += iDr  *(2.0     *UR - 2.0     *Q2)/R ;
+  e +=       (1.0     *UR - 1.45    *Q2)/R2;
+  e +=  Dr  *(0.7     *UR - 1.15    *Q2)/R3;
+  e +=  Dr2 *(0.55    *UR - 0.981786*Q2)/R4;
+  e +=  Dr3 *(0.469286*UR - 0.882857*Q2)/R5;
+  e +=  Dr4 *(0.423214*UR - 0.820839*Q2)/R6;
+	// higher-order terms emphasize the singular nature of the
+	// solution near the poles, so better to truncate the series
 	
 	/*-------------------------------------------------*/
 	
@@ -145,36 +138,39 @@ void func(int ny, double Ca,
   	double sinr = sin/r;
 
   	// calculate function
-  	f[0] = -sin;
-  	f[1] = -cs;
-  	f[2] = sinr*cosr
-  	     + cs*sinr 
-  	     - qs;
-  	f[3] = p
-  	     - sig*(cs - cosr)
-  	     + 0.5*(cs + cosr)*(cs*cs - cosr*cosr)
-  	     + qs*sinr;
-  	f[4] = Ca*g*cos;
-  	f[5] = -Ca*e;
-  	f[6] = 2*M_PI*r;
-  	f[7] = M_PI*r2*cos;
-		f[8] = 0;
-  	f[9] = 0;
+  	f[0]  = -sin                                                                ;
+		f[1]  =  cos                                                                ;
+  	f[2]  = -cs                                                                 ;
+  	f[3]  =  sinr*cosr + cs*sinr - qs                                           ;
+  	f[4]  =  p - sig*(cs - cosr) + 0.5*(cs + cosr)*(cs*cs - cosr*cosr) + qs*sinr;
+  	f[5]  =  T0*g*cos                                                           ;
+  	f[6]  = -T0*e                                                               ;
+  	f[7]  =  2.0*M_PI*r                                                         ;
+  	f[8]  =  M_PI*r2*cos                                                        ;
+		f[9]  =  (-r*ur + UR*tana)*cos/R0                                           ;
+  	f[10] = -tana*cos                                                           ;
+		f[11] =  M_PI*r2*x*cos/((4.0/3.0)*M_PI*v)                                   ;
+		f[12] =  0.0                                                                ;
+		f[13] =  0.0                                                                ;
 	}
 	else { // approximate cs, cphi as constant (spherical cap)
   	double cos = gsl_sf_cos(psi);
   	double sin = gsl_sf_sin(psi);
-		
-		f[0] = -sin;
-		f[1] = -cs;
-		f[2] = 0;
-		f[3] = 0.5*(p - 2.0*cs*sig);
-		f[4] = Ca*g*cos; 
-		f[5] = -Ca*e;
-		f[6] = 2.0*M_PI*r;
-		f[7] = M_PI*r*r*cos;
-		f[8] = 0; 
-		f[9] = 0;
+
+  	f[0]  = -sin                                                                ;
+		f[1]  =  cos                                                                ;
+  	f[2]  = -cs                                                                 ;
+  	f[3]  =  0.0                                                                ;
+  	f[4]  =  0.5*(p - 2.0*cs*sig);                                              ;
+  	f[5]  =  T0*g*cos                                                           ;
+  	f[6]  = -T0*e                                                               ;
+  	f[7]  =  2.0*M_PI*r                                                         ;
+  	f[8]  =  M_PI*r2*cos                                                        ;
+		f[9]  =  (-r*ur + UR*tana)*cos/R0                                           ;
+  	f[10] = -tana*cos                                                           ;
+		f[11] =  M_PI*r2*x*cos/((4.0/3.0)*M_PI*v)                                   ;
+		f[12] =  0.0                                                                ;
+		f[13] =  0.0                                                                ;
 	}
 
 	for (i = 0; i < ny; i++){
